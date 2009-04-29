d33060c4a105ebde133229a48110dcfa0808ea82
Tim van Werkhoven <t.i.m.vanwerkhoven@xs4all.nl>
2009-04-29 15:20:38 +0200

    Set boxstyle to empty, commented out old plot routines.

--- libplot.py
+++ libplot.py
@@ -83,7 +83,11 @@ def plotShifts(filebase, shifts, sapos, sasize, sfpos, sfsize, plorigin=(0,0), p
 	# Output filenames
 	plotfile = filebase + '.eps'
 	infofile =  filebase + '-info.txt'
-	
+	# Dimensions
+	nfile = shifts.shape[0]
+	nref = shifts.shape[1]
+	nsa = shifts.shape[2]
+	nsf = shifts.shape[3]
 	# Average over Nref, the number of references we have
 	shifts = shifts.mean(axis=1)
 	plrange = N.array(plrange)
@@ -92,9 +96,12 @@ def plotShifts(filebase, shifts, sapos, sasize, sfpos, sfsize, plorigin=(0,0), p
 	# Reform data, save to file. Use 4 column x, y, dx, dy syntax.
 	allcpos = (sapos.reshape(-1,1,2) + sfpos.reshape(1,-1,2)) + sfsize/2.0
 	allcpos = allcpos.reshape(-1,2)
+	shiftsplall = (shifts.reshape(nfile,-1,2) * mag) + allcpos.reshape(1,-1, 2)
+	shiftsplall = shiftsplall.reshape(-1, 2)
 	shiftspl = (N.mean(shifts, axis=0)).reshape(-1,2) * mag
 	datafile = os.path.join('/tmp', 'tmpgnuplot-%f' % (time.time()))
 	N.savetxt(datafile, N.concatenate((allcpos, shiftspl), axis=1))
+	delfiles = [datafile]
 	
 	# Init plotting
 	gp = Gnuplot.Gnuplot()
@@ -117,14 +124,14 @@ def plotShifts(filebase, shifts, sapos, sasize, sfpos, sfsize, plorigin=(0,0), p
 	for _sa in xrange(len(sapos)):
 		_sapos = sapos[_sa]
 		# Draw subaperture boxes
-		gp('set obj %d rect from %d,%d to %d,%d lw 0.4' % \
+		gp('set obj %d rect from %d,%d to %d,%d fs empty lw 0.4' % \
 			(box, _sapos[0], _sapos[1], _sapos[0]+sasize[0], _sapos[1]+sasize[1]))
 		box += 1
 		
 		for _sf in xrange(len(sfpos)):
 			_sfpos = _sapos + sfpos[_sf]
 			# Draw subfield boxes
-			gp('set obj %d rect from %d,%d to %d,%d lw 0.2' % \
+			gp('set obj %d rect from %d,%d to %d,%d fs empty lw 0.2' % \
 				(box, _sfpos[0], _sfpos[1], _sfpos[0]+sfsize[0], _sfpos[1]+sfsize[1]))
 			box += 1
 			
@@ -147,8 +154,19 @@ def plotShifts(filebase, shifts, sapos, sasize, sfpos, sfsize, plorigin=(0,0), p
 		gp('set label "%d-pix shift" at %d,%d font "Palatino,4"' % \
 			(avgshift, txtpos[0], txtpos[1]))
 	
-	gp('plot "%s" using 1:2:3:4 with vectors nohead lw 0.5 title "WFWFS shifts"'%\
-		(datafile))
+	# If allsh is True, add dots for all individual measured shifts
+	gp('set style line 1 lt 1 lw 0.6 lc rgb "red"')
+	gp('set style line 2 pt 0 ps 0.6 lt 0 lw 0.6 lc rgb "blue"')
+	if (allsh is True):
+		datafilea = os.path.join('/tmp', 'tmpgnuplot-%f' % (time.time()))
+		N.savetxt(datafilea, shiftsplall)
+		delfiles.append(datafilea)
+		gp('plot "%s" with points ls 2 title "All WFWFS shifts", "%s" using 1:2:3:4 with vectors nohead ls 1 title "WFWFS shifts"' % \
+		 	(datafilea, datafile))
+	else:
+		# Plot the actual vectors from file now
+		gp('plot "%s" using 1:2:3:4 with vectors nohead ls 1 title "WFWFS shifts"' %\
+		 	(datafile))
 	
 	waitForFile(plotfile)
 	convertPdf(plotfile)
@@ -173,182 +191,182 @@ def plotShifts(filebase, shifts, sapos, sasize, sfpos, sfsize, plorigin=(0,0), p
 	f.close()
 	
 	# Remove datafiles
-	rmFiles([datafile])
+	#rmFiles(delfiles)
 	# Done
 
 
-def procStatShift(plotfile, shvec, pos, sasize, plorigin=(0,0), plrange=(2048, 2048), mag=1.0, allsh=False, title=None, legend=True, avgshift=True):
-	"""
-	Process static shift information. This is meant to process some data of 
-	Phase 1 analysis in libtomo.py. 
-	
-	'shvec' should be an N x NREF x SA x 2 array, with N the number of frames, 
-	NREF the number of reference subapertures used, SA the number of 
-	subapertures. 'pos' should be a SA x 2 array. This routine plots the 
-	averages 'shvec' such that a SA x 2 array is left, and plots these vectors 
-	at the positions listed by 'pos'.
-	
-	Additional options include (default values in brackets):
-	'plrange': sets the plotting region [(2048, 2048)]
-	'plorigin': sets the plotting origin [(0, 0)]
-	'mag': magnify each vector with a scalar [1.0]
-	'allsh': if True, do not only plot the average shift, but also plot small 
-	dots for the individual shifts. [False]
-	'title': optional title for plot. [None]
-	'legend': if True, add an arrow with a length corresponding to the average 
-	shift rounded to a pixel but never smaller than 1 pixel. [True]
-	'avgshift': if True, print the average shift in each subaperture. [True]
-	"""
-	
-	### Process data
-	### ============
-	
-	# Fix lists to numpy lists for easier manipulation
-	plorigin = N.array(plorigin)
-	plrange = N.array(plrange)
-	# Calculate both the lower-left position and the centroid position here.
-	# Add sasize/2 to lower-left position to get centroid.
-	llpos = pos
-	cpos = pos + sasize.reshape(1,-1)/2	
-	
-	# Get the sizes of the data
-	n = shvec.shape[0]
-	nref = shvec.shape[1]
-	nsa = shvec.shape[2]
-	
-	# shvec.shape[3] should be 1, reshape this away
-	shvec = shvec.reshape(n, nref, nsa, 2)
-	
-	# Subtract average shift per frame from each frame:
-	shfravg = N.mean(shvec, axis=2) 	# this gives a N x NREF x 2 array
-	#shfrrefavg = N.mean(shfravg, axis=1)		# this gives a N x 2 array
-	shnrm = shvec - shfravg.reshape(n,nref,1,2)
-	# Magnify plots a bit
-	# shnrmpl = shnrm * mag
-	# # Now average over all frames:
-	shavg = shnrm.mean(axis=0)	# this gives an SA x 2 array
-	# # Magnify the shift vectors, if requested:
-	# shavgpl = shavg * mag
-	
-	### Plot a map for each reference
-	### =============================
-	
-	# Check if the sizes match
-	if ((nsa, 2) != cpos.shape):
-		raise ValueError("'shvec' and 'pos' are not of the same shape.")
-	
-	# Plot all individual reference-shifts
-	for _ref in range(nref):
-		# Add extension to filename (-sing-0,1,2...)
-		pf = os.path.splitext(plotfile)
-		_plotStatShift(pf[0] + "-sing-%d" % (_ref) + pf[1], shnrm[:,_ref], cpos, llpos, sasize, plorigin, plrange, mag, allsh, title, legend, avgshift)
-	
-	# Now plot the grand average
-	_plotStatShift(plotfile, N.mean(shnrm, axis=1), cpos, llpos, sasize, plorigin, plrange, mag, allsh, title, legend, avgshift)
-
-		
-def _plotStatShift(plotfile, shnrm, cpos, llpos, sasize, plorigin, plrange, mag, allsh, title, legend, avgshift):
-	"""
-	Helper function for procStatShift(). This does all the plotting.
-	"""
-	# Magnify plots a bit
-	shnrmpl = shnrm * mag
-	# Now average over all frames:
-	shavg = shnrm.mean(axis=0)	# this gives an SA x 2 array
-	# Magnify the shift vectors, if requested:
-	shavgpl = shavg * mag
-	
-	# TODO: rmFiles fails when filenames are not set, can be solved by setting 
-	# files to bogus strings (like here). What's a better solution?
-	datafilea = ''
-	datafile = ''
-	
-	# Initiate Gnuplot
-	gp = Gnuplot.Gnuplot()
-	
-	# Set default settings
-	gnuplotInit(gp, hardcopy=plotfile, rmfile=True)
-	
-	# Save data to temporary file. Use 4 column x, y, dx, dy syntax.
-	datafile = os.path.join('/tmp', 'tmpgnuplot-%f' % (time.time()))
-	N.savetxt(datafile, N.concatenate((cpos, shavgpl), axis=1))
-	
-	# Set plotregion
-	gp('set xrange [%d:%d]' % (plorigin[0], plrange[0]))
-	gp('set yrange [%d:%d]' % (plorigin[1], plrange[1]))
-	# Set axes label
-	gp('set xlabel "x [pixel]"')
-	gp('set ylabel "y [pixel]"')
-	# Set aspect ratio (probably square)
-	plsize = plrange-plorigin
-	gp('set size ratio %f' % (1.0*plsize[1]/plsize[0]))
-	# Disable legend
-	gp('set key off')
-	
-	# Set title
-	if (title != None):
-		title = addGpSlashes(title)
-		gp('set title "%s"' % (title))
-		
-	# If sasize is not 'None', plot boxes (rectangles) in the plot region
-	if (sasize != None):
-		sasize = N.array(sasize)
-		if (sasize.shape[0] != 2 or sasize.ndim != 1):
-			prNot(VERB_WARN, "plotVecMap(): Cannot add boxes around vectors, is not a valid 2-element list")
-		
-		box = 1
-		for p in llpos:
-			gp('set obj %d rect from %d,%d to %d,%d' % (box, p[0], p[1], \
-			 	p[0]+sasize[0], p[1]+sasize[1]))
-			box += 1
-			
-	# If avgshift is True, add the average shift as caption
-	if (avgshift is True):
-		for sa in xrange(len(llpos)):
-			# Set caption
-			cap = '#%d (%.3g, %.3g)' % \
-				(sa, shavg[sa][0], shavg[sa][1])
-			gp('set label "%s" at %d,%d font "Palatino,4"' % \
-				(cap, llpos[sa][0] + sasize[0]*0.06, \
-					llpos[sa][1] + sasize[1]*0.1))
-					
-	# If legend is True, add an arrow with a length corresponding to the 
-	# average shift:
-	if (legend is True):
-		# Average shift for all subapertures to get the global average shift:
-		avgshift = legend #N.round(N.mean(shavg))
-		if (avgshift < 1.0): avgshift = 1.0
-		legsize = avgshift * mag
-		# Position at 5% of the plot
-		legpos = N.round(plsize * 0.05)
-		txtpos = N.round(plsize * [0.05, 0.02])
-		gp('set style line 1 lt 1 lw 1.0 lc rgb "red"')
-		gp('set arrow from %d,%d rto %d,%d ls 1' % \
-			(legpos[0], legpos[1], legsize, 0))
-		gp('set label "%d-pix shift" at %d,%d font "Palatino,4"' % \
-			(avgshift, txtpos[0], txtpos[1]))
-			
-	# If allsh is True, add dots for all individual measured shifts
-	if (allsh is True):
-		datafilea = os.path.join('/tmp', 'tmpgnuplot-%f' % (time.time()))
-		allshvec = shnrmpl + cpos.reshape(1,-1,2)
-		allshvec = allshvec.reshape(-1,2)
-		N.savetxt(datafilea, allshvec)
-		gp('set style line 1 lt 1 lw 1.0 lc rgb "red"')
-		gp('set style line 2 pt 0 ps 0.8 lt 0 lw 0.8 lc rgb "blue"')
-		gp('plot "%s" with points ls 2 title "All WFWFS shifts", "%s" using 1:2:3:4 with vectors ls 1 title "WFWFS shifts"' % \
-		 	(datafilea, datafile))
-	else:
-		# Plot the actual vectors from file now
-		gp('plot "%s" using 1:2:3:4 with vectors ls 1 title "WFWFS shifts"' %\
-		 	(datafile))
-	
-	waitForFile(plotfile)
-	convertPdf(plotfile)
-	# Remove datafiles
-	rmFiles([datafile, datafilea])
-	# Done
-
+# def procStatShift(plotfile, shvec, pos, sasize, plorigin=(0,0), plrange=(2048, 2048), mag=1.0, allsh=False, title=None, legend=True, avgshift=True):
+# 	"""
+# 	Process static shift information. This is meant to process some data of 
+# 	Phase 1 analysis in libtomo.py. 
+# 	
+# 	'shvec' should be an N x NREF x SA x 2 array, with N the number of frames, 
+# 	NREF the number of reference subapertures used, SA the number of 
+# 	subapertures. 'pos' should be a SA x 2 array. This routine plots the 
+# 	averages 'shvec' such that a SA x 2 array is left, and plots these vectors 
+# 	at the positions listed by 'pos'.
+# 	
+# 	Additional options include (default values in brackets):
+# 	'plrange': sets the plotting region [(2048, 2048)]
+# 	'plorigin': sets the plotting origin [(0, 0)]
+# 	'mag': magnify each vector with a scalar [1.0]
+# 	'allsh': if True, do not only plot the average shift, but also plot small 
+# 	dots for the individual shifts. [False]
+# 	'title': optional title for plot. [None]
+# 	'legend': if True, add an arrow with a length corresponding to the average 
+# 	shift rounded to a pixel but never smaller than 1 pixel. [True]
+# 	'avgshift': if True, print the average shift in each subaperture. [True]
+# 	"""
+# 	
+# 	### Process data
+# 	### ============
+# 	
+# 	# Fix lists to numpy lists for easier manipulation
+# 	plorigin = N.array(plorigin)
+# 	plrange = N.array(plrange)
+# 	# Calculate both the lower-left position and the centroid position here.
+# 	# Add sasize/2 to lower-left position to get centroid.
+# 	llpos = pos
+# 	cpos = pos + sasize.reshape(1,-1)/2	
+# 	
+# 	# Get the sizes of the data
+# 	n = shvec.shape[0]
+# 	nref = shvec.shape[1]
+# 	nsa = shvec.shape[2]
+# 	
+# 	# shvec.shape[3] should be 1, reshape this away
+# 	shvec = shvec.reshape(n, nref, nsa, 2)
+# 	
+# 	# Subtract average shift per frame from each frame:
+# 	shfravg = N.mean(shvec, axis=2) 	# this gives a N x NREF x 2 array
+# 	#shfrrefavg = N.mean(shfravg, axis=1)		# this gives a N x 2 array
+# 	shnrm = shvec - shfravg.reshape(n,nref,1,2)
+# 	# Magnify plots a bit
+# 	# shnrmpl = shnrm * mag
+# 	# # Now average over all frames:
+# 	shavg = shnrm.mean(axis=0)	# this gives an SA x 2 array
+# 	# # Magnify the shift vectors, if requested:
+# 	# shavgpl = shavg * mag
+# 	
+# 	### Plot a map for each reference
+# 	### =============================
+# 	
+# 	# Check if the sizes match
+# 	if ((nsa, 2) != cpos.shape):
+# 		raise ValueError("'shvec' and 'pos' are not of the same shape.")
+# 	
+# 	# Plot all individual reference-shifts
+# 	for _ref in range(nref):
+# 		# Add extension to filename (-sing-0,1,2...)
+# 		pf = os.path.splitext(plotfile)
+# 		_plotStatShift(pf[0] + "-sing-%d" % (_ref) + pf[1], shnrm[:,_ref], cpos, llpos, sasize, plorigin, plrange, mag, allsh, title, legend, avgshift)
+# 	
+# 	# Now plot the grand average
+# 	_plotStatShift(plotfile, N.mean(shnrm, axis=1), cpos, llpos, sasize, plorigin, plrange, mag, allsh, title, legend, avgshift)
+# 
+# 		
+# def _plotStatShift(plotfile, shnrm, cpos, llpos, sasize, plorigin, plrange, mag, allsh, title, legend, avgshift):
+# 	"""
+# 	Helper function for procStatShift(). This does all the plotting.
+# 	"""
+# 	# Magnify plots a bit
+# 	shnrmpl = shnrm * mag
+# 	# Now average over all frames:
+# 	shavg = shnrm.mean(axis=0)	# this gives an SA x 2 array
+# 	# Magnify the shift vectors, if requested:
+# 	shavgpl = shavg * mag
+# 	
+# 	# TODO: rmFiles fails when filenames are not set, can be solved by setting 
+# 	# files to bogus strings (like here). What's a better solution?
+# 	datafilea = ''
+# 	datafile = ''
+# 	
+# 	# Initiate Gnuplot
+# 	gp = Gnuplot.Gnuplot()
+# 	
+# 	# Set default settings
+# 	gnuplotInit(gp, hardcopy=plotfile, rmfile=True)
+# 	
+# 	# Save data to temporary file. Use 4 column x, y, dx, dy syntax.
+# 	datafile = os.path.join('/tmp', 'tmpgnuplot-%f' % (time.time()))
+# 	N.savetxt(datafile, N.concatenate((cpos, shavgpl), axis=1))
+# 	
+# 	# Set plotregion
+# 	gp('set xrange [%d:%d]' % (plorigin[0], plrange[0]))
+# 	gp('set yrange [%d:%d]' % (plorigin[1], plrange[1]))
+# 	# Set axes label
+# 	gp('set xlabel "x [pixel]"')
+# 	gp('set ylabel "y [pixel]"')
+# 	# Set aspect ratio (probably square)
+# 	plsize = plrange-plorigin
+# 	gp('set size ratio %f' % (1.0*plsize[1]/plsize[0]))
+# 	# Disable legend
+# 	gp('set key off')
+# 	
+# 	# Set title
+# 	if (title != None):
+# 		title = addGpSlashes(title)
+# 		gp('set title "%s"' % (title))
+# 		
+# 	# If sasize is not 'None', plot boxes (rectangles) in the plot region
+# 	if (sasize != None):
+# 		sasize = N.array(sasize)
+# 		if (sasize.shape[0] != 2 or sasize.ndim != 1):
+# 			prNot(VERB_WARN, "plotVecMap(): Cannot add boxes around vectors, is not a valid 2-element list")
+# 		
+# 		box = 1
+# 		for p in llpos:
+# 			gp('set obj %d rect from %d,%d to %d,%d' % (box, p[0], p[1], \
+# 			 	p[0]+sasize[0], p[1]+sasize[1]))
+# 			box += 1
+# 			
+# 	# If avgshift is True, add the average shift as caption
+# 	if (avgshift is True):
+# 		for sa in xrange(len(llpos)):
+# 			# Set caption
+# 			cap = '#%d (%.3g, %.3g)' % \
+# 				(sa, shavg[sa][0], shavg[sa][1])
+# 			gp('set label "%s" at %d,%d font "Palatino,4"' % \
+# 				(cap, llpos[sa][0] + sasize[0]*0.06, \
+# 					llpos[sa][1] + sasize[1]*0.1))
+# 					
+# 	# If legend is True, add an arrow with a length corresponding to the 
+# 	# average shift:
+# 	if (legend is True):
+# 		# Average shift for all subapertures to get the global average shift:
+# 		avgshift = legend #N.round(N.mean(shavg))
+# 		if (avgshift < 1.0): avgshift = 1.0
+# 		legsize = avgshift * mag
+# 		# Position at 5% of the plot
+# 		legpos = N.round(plsize * 0.05)
+# 		txtpos = N.round(plsize * [0.05, 0.02])
+# 		gp('set style line 1 lt 1 lw 1.0 lc rgb "red"')
+# 		gp('set arrow from %d,%d rto %d,%d ls 1' % \
+# 			(legpos[0], legpos[1], legsize, 0))
+# 		gp('set label "%d-pix shift" at %d,%d font "Palatino,4"' % \
+# 			(avgshift, txtpos[0], txtpos[1]))
+# 			
+# 	# If allsh is True, add dots for all individual measured shifts
+# 	if (allsh is True):
+# 		datafilea = os.path.join('/tmp', 'tmpgnuplot-%f' % (time.time()))
+# 		allshvec = shnrmpl + cpos.reshape(1,-1,2)
+# 		allshvec = allshvec.reshape(-1,2)
+# 		N.savetxt(datafilea, allshvec)
+# 		gp('set style line 1 lt 1 lw 1.0 lc rgb "red"')
+# 		gp('set style line 2 pt 0 ps 0.8 lt 0 lw 0.8 lc rgb "blue"')
+# 		gp('plot "%s" with points ls 2 title "All WFWFS shifts", "%s" using 1:2:3:4 with vectors ls 1 title "WFWFS shifts"' % \
+# 		 	(datafilea, datafile))
+# 	else:
+# 		# Plot the actual vectors from file now
+# 		gp('plot "%s" using 1:2:3:4 with vectors ls 1 title "WFWFS shifts"' %\
+# 		 	(datafile))
+# 	
+# 	waitForFile(plotfile)
+# 	convertPdf(plotfile)
+# 	# Remove datafiles
+# 	rmFiles([datafile, datafilea])
+# 	# Done
+# 
 
 def overlayMask(img, saccdpos, saccdsize, filename, number=True, coord=True, norm=0.5, crop=False, border=True):
 	"""
@@ -687,7 +705,7 @@ def showSaSfLayout(outfile, sapos, sasize, sfpos=[], sfsize=[], method='ccd', co
 		obj += 1
 		sanum += 1
 		# Set subimage box
-		gp('set obj %d rect from %f,%f to %f,%f lw 0.8' % \
+		gp('set obj %d rect from %f,%f to %f,%f fs empty lw 0.8' % \
 			(obj, sa[0], sa[1], sa[0]+sasize[0], sa[1]+sasize[1]))
 		
 		caption = ''
@@ -705,7 +723,7 @@ def showSaSfLayout(outfile, sapos, sasize, sfpos=[], sfsize=[], method='ccd', co
 		for sf in sfpos:
 			_sf = sa + sf
 			obj += 1
-			gp('set obj %d rect from %f,%f to %f,%f lw 0.4' % \
+			gp('set obj %d rect from %f,%f to %f,%f fs empty lw 0.4' % \
 				(obj, _sf[0], _sf[1], _sf[0]+sfsize[0], _sf[1]+sfsize[1]))
 	
 	# Finish the plot (TODO: ugly hack, how to do this nicer?)
