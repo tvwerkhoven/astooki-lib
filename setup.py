c734d4fba8842106dde1a1d41db46540bd05f4b3
Tim van Werkhoven <t.i.m.vanwerkhoven@xs4all.nl>
2009-12-24 13:12:32 +0100

    New milestone, no known bugs.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup.py -- setup file for the astooki-lib module
 Created by Tim van Werkhoven (t.i.m.vanwerkhoven@xs4all.nl) on 2009-05-20.
 Copyright (c) 2009 Tim van Werkhoven. All rights reserved.
 """
-GITREVISION="v20090626.0-30-gee9c96c"
+GITREVISION="v20090626.0-31-g10ba0e2"
 import sys
 
 # Try importing to see if we have NumPy available (we need this)
